3a0761ff3edcdad2fc0965a6c71c11a0161139ee
Graham Ashton <graham@effectif.com>
2010-11-14 23:59:48 +0000

    Support the new Sinatra::Base style app.

--- app.rb
+++ app.rb
@@ -1,7 +1,13 @@
 # Use the app.rb file to load Ruby code, modify or extend the models, or
 # do whatever else you fancy when the theme is loaded.
 
-# This code tells Nesta to look for assets in the theme's public folder.
-# Highly recommended if your theme contains images or JavaScript.
-#
-use Rack::Static, :urls => ["/original"], :root => "themes/original/public"
+module Nesta
+  class App
+    use Rack::Static, :urls => ["/original"], :root => "themes/original/public"
+
+    helpers do
+      # Add new helpers here.
+    end
+  end
+end
+
